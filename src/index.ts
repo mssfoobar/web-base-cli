8a7af52e9b313029693b8e8844bc7126d495c68f
jarylozh <jarylozh@gmail.com>
2024-10-08 10:23:24 +0800

    feat: make public and private routes optional inputs

--- src/index.ts
+++ src/index.ts
@@ -1,146 +1,158 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-import { mkdir, cp, rm, copyFile, writeFile, rmSync } from 'fs';
-import { promptPackageRoutes, type Module, getWorkingDirectory } from './utils';
-import { spinner, text } from '@clack/prompts';
-import { exec, execSync } from 'child_process';
-import { exit } from 'process';
+import { Command } from "commander";
+import { mkdir, cp, rm, copyFile, writeFile, rmSync } from "fs";
+import { promptPackageRoutes, type Module, getWorkingDirectory } from "./utils";
+import { spinner, text } from "@clack/prompts";
+import { exec, execSync } from "child_process";
+import { exit } from "process";
 
-let map: Map<string, any> = new Map([['dash', { project: 'aoh', url: '@mssfoobar/dash@0.0.1' }]]);
+let map: Map<string, any> = new Map([
+  ["dash", { project: "aoh", url: "@mssfoobar/dash@0.0.1" }],
+]);
 
 const program: Command = new Command();
 
 program
-	.name('CLI')
-	.version('1.0.0')
-	.description('This is the CLI to install modules onto your web-base framework')
-	.command('package')
-	.action(async (_: string) => {
-		let module: Module = await promptPackageRoutes();
+  .name("CLI")
+  .version("1.0.0")
+  .description(
+    "This is the CLI to install modules onto your web-base framework"
+  )
+  .command("package")
+  .action(async (_: string) => {
+    let module: Module = await promptPackageRoutes();
+    const libPath: string = `${module.lib}`;
+    const org: string = "mssfoobar";
 
-		const libPath: string = `${module.lib}`;
-		const publicRoutePath: string = `${module.public}`;
-		const privateRoutePath: string = `${module.private}`;
-		const org: string = 'mssfoobar';
+    let s = spinner();
+    s.start("Copying files...");
+    rmSync(`${module.web}/mod`, { recursive: true, force: true });
 
-		let s = spinner();
-		s.start('Copying files...');
-		rmSync(`${module.web}/mod`, { recursive: true, force: true });
-		//make a directory
-		await mkdir(`${module.web}/mod`, (_) => (_ ? console.error(_) : null));
+    //make a directory
+    await mkdir(`${module.web}/mod`, (_) => (_ ? console.error(_) : null));
 
-		//copy the necessary files over
-		await cp(
-			libPath,
-			`${module.web}/mod/src/lib/${module.project}/${module.name}`,
-			{ recursive: true },
-			(_) => (_ ? console.error(_) : null)
-		);
-		await cp(
-			publicRoutePath,
-			`${module.web}/mod/src/routes/(public)/${module.project}/${module.name}`,
-			{ recursive: true },
-			(_) => (_ ? console.error(_) : null)
-		);
-		await cp(
-			privateRoutePath,
-			`${module.web}/mod/src/routes/(private)/${module.project}/${module.name}`,
-			{ recursive: true },
-			(_) => (_ ? console.error(_) : null)
-		);
+    //copy the necessary files over
+    await cp(
+      libPath,
+      `${module.web}/mod/src/lib/${module.project}/${module.name}`,
+      { recursive: true },
+      (_) => (_ ? console.error(_) : null)
+    );
 
-		s.stop();
+    //Skip copying files if no input
+    if (module.public) {
+      const publicRoutePath: string = `${module.public}`;
+      await cp(
+        publicRoutePath,
+        `${module.web}/mod/src/routes/(public)/${module.project}/${module.name}`,
+        { recursive: true },
+        (_) => (_ ? console.error(_) : null)
+      );
+    }
 
-		// //initialize package.json
-		const packageFile = require(`${module.package}`);
-		if (!packageFile.repository) {
-			await text({
-				message: 'Repository not found. Please enter a repository url',
-				defaultValue: '',
-				validate: (value: string) => {
-					if (!value) return 'Please enter a repository url';
-					packageFile.repository = {
-						type: 'git',
-						url: value
-					};
-				}
-			});
-		}
+    if (module.private) {
+      const privateRoutePath: string = `${module.private}`;
+      await cp(
+        privateRoutePath,
+        `${module.web}/mod/src/routes/(private)/${module.project}/${module.name}`,
+        { recursive: true },
+        (_) => (_ ? console.error(_) : null)
+      );
+    }
 
-		let s1 = spinner();
-		s1.start('initializing package.json');
-		await setTimeout(() => {}, 100);
-		delete packageFile['devDependencies'];
-		delete packageFile['scripts'];
+    s.stop();
 
-		packageFile.name = `@${org}/${module.name}`;
-		packageFile.private = false;
-		packageFile.publishConfig = {};
-		packageFile.publishConfig[`@${org}:registry`] = 'https://npm.pkg.github.com';
-		writeFile(`${module.web}/mod/package.json`, JSON.stringify(packageFile, null, 2), (_) =>
-			_ ? console.error(_) : null
-		);
+    //initialize package.json
+    const packageFile = require(`${module.package}`);
+    if (!packageFile.repository) {
+      await text({
+        message: "Repository not found. Please enter a repository url",
+        defaultValue: "",
+        validate: (value: string) => {
+          if (!value) return "Please enter a repository url";
+          packageFile.repository = {
+            type: "git",
+            url: value,
+          };
+        },
+      });
+    }
 
-		s1.message('publishing package...');
-		let process = await exec(`npm publish ${module.web}/mod`, (_) => {
-			if (_) {
-				console.error(_);
-				exit(0);
-			}
-		});
+    let s1 = spinner();
+    s1.start("initializing package.json");
+    await setTimeout(() => {}, 100);
+    delete packageFile["devDependencies"];
+    delete packageFile["scripts"];
 
-		process.on('close', (code) => {
-			s1.stop(
-				`Package "${module.name}" published to github successfully. You may now install using \`cli install ${module.name}\``
-			);
-		});
-	});
+    packageFile.name = `@${org}/${module.name}`;
+    packageFile.private = false;
+    packageFile.publishConfig = {};
+    packageFile.publishConfig[`@${org}:registry`] =
+      "https://npm.pkg.github.com";
+    writeFile(
+      `${module.web}/mod/package.json`,
+      JSON.stringify(packageFile, null, 2),
+      (_) => (_ ? console.error(_) : null)
+    );
 
-	program
-	.command('install')
-	.argument('<module>')
-	.action(async (module: string) => {
-		if (!map.has(module)) {
-			console.error(`Error: ${module} not found in the list of modules`);
-			exit(0);
-		}
+    s1.message("publishing package...");
+    let process = await exec(`npm publish ${module.web}/mod`, (_) => {
+      if (_) {
+        console.error(_);
+        exit(0);
+      }
+    });
 
-		let root: string = await getWorkingDirectory();
-		await text({message: 'Please enter the path to your root folder', initialValue: `${root}`});
+    process.on("close", (code) => {
+      s1.stop(
+        `Package "${module.name}" published to github successfully. You may now install using \`cli install ${module.name}\``
+      );
+    });
+  });
 
-		let project: string = map.get(module).project;
 
-		let s = spinner();
-		s.start('installing module...');
-		setTimeout(() => {}, 5000);
-		s.stop();
 
-		s = spinner();
-		s.start('installing dependencies...');
-		let process = await exec(`npm install ${map.get(module).url}`, (exec) => {
+//This is for installing a module
+program
+  .command("install")
+  .argument("<module>")
+  .action(async (module: string) => {
+    if (!map.has(module)) {
+      console.error(`Error: ${module} not found in the list of modules`);
+      exit(0);
+    }
 
-			if (exec) console.log(exec);
-			cp(
-			`${root}/node_modules/@mssfoobar/${module}/src`,
-			`${root}/src`,
-			{ recursive: true },
-			(err) => {
-				if (err) console.log(err);
-			}
-		);
+    let root: string = await getWorkingDirectory();
+    await text({
+      message: "Please enter the path to your root folder",
+      initialValue: `${root}`,
+    });
 
-		});
+    let project: string = map.get(module).project;
 
-		process.on('close', (code) => {
-			s.stop(`installing ${module} completed`);
-		})
+    let s = spinner();
+    s.start("installing module...");
+    setTimeout(() => {}, 5000);
+    s.stop();
 
-		
-	});
+    s = spinner();
+    s.start("installing dependencies...");
+    let process = await exec(`npm install ${map.get(module).url}`, (exec) => {
+      if (exec) console.log(exec);
+      cp(
+        `${root}/node_modules/@mssfoobar/${module}/src`,
+        `${root}/src`,
+        { recursive: true },
+        (err) => {
+          if (err) console.log(err);
+        }
+      );
+    });
 
+    process.on("close", (code) => {
+      s.stop(`installing ${module} completed`);
+    });
+  });
 
 program.parse();
-
-
-
--- src/utils.ts
+++ src/utils.ts
@@ -1,115 +1,125 @@
-import { exit } from 'process';
-import { exec } from 'child_process';
-import { promisify } from 'util';
-import { text, group, isCancel } from '@clack/prompts';
+import { exit } from "process";
+import { exec } from "child_process";
+import { promisify } from "util";
+import { text, group, isCancel, confirm } from "@clack/prompts";
 const execProm = promisify(exec);
 
 export async function getGitRepoRoot(): Promise<string> {
-	const command = 'git rev-parse --show-toplevel';
-	let path;
-	try {
-		const { stdout, stderr } = await execProm(command);
-		path = stdout;
-	} catch (error) {
-		console.error(`Error: ${error}`);
-		exit();
-	}
+  const command = "git rev-parse --show-toplevel";
+  let path;
+  try {
+    const { stdout, stderr } = await execProm(command);
+    path = stdout;
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    exit();
+  }
 
-	return path.trim(); // remove trailing newline
+  return path.trim(); // remove trailing newline
 }
 
 export async function getWorkingDirectory(): Promise<string> {
-	let os = process.platform;
-	const command: string = (os === 'darwin' || os === 'linux') ? `pwd` : `cd`;
-	let path;
-	try {
-		const { stdout, stderr } = await execProm(command);
-		path = stdout;
-	} catch (error) {
-		console.error(`Error: ${error}`);
-		exit();
-	}
+  let os = process.platform;
+  const command: string = os === "darwin" || os === "linux" ? `pwd` : `cd`;
+  let path;
+  try {
+    const { stdout, stderr } = await execProm(command);
+    path = stdout;
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    exit();
+  }
 
-	return path.trim(); // remove trailing newline
+  return path.trim(); // remove trailing newline
 }
 
 export type Module = {
-	name: string;
-	project: string;
-	web: string;
-	lib: string;
-	public: string;
-	private: string;
-	package: string;
+  name: string;
+  project: string;
+  web: string;
+  lib: string;
+  public?: string;
+  private?: string;
+  package: string;
 };
 
 export async function promptPackageRoutes(): Promise<Module> {
-	const DEFAULT_PROJECT: string = 'aoh';
-	const cwd: string = await getWorkingDirectory();
-	let package_name: string;
-	let project_name: string;
-	let web_route: string;
+  const DEFAULT_PROJECT: string = "aoh";
+  const cwd: string = await getWorkingDirectory();
+  let package_name: string = "";
+  let project_name: string = "";
+  let web_route: string = "";
 
-	const project = await group({
-		name: async () =>
-			await text({
-				message: 'Please enter the package name',
-				validate: (value: string) => {
-					if (isCancel(value)) exit(0);
-					if (!value) {
-						return 'Please enter a package name';
-					}
+  const project = await group({
+    name: async () =>
+      await text({
+        message: "Please enter the package name",
+        validate: (value: string) => {
+          if (isCancel(value)) exit(0);
+          if (!value) {
+            return "Please enter a package name";
+          }
 
-					package_name = value.replace('/', '');
-				}
-			}),
-		project: async () =>
-			await text({
-				message: `Please enter the project name`,
-				validate: (value: string) => {
-					if (isCancel(value)) exit(0);
-					if (!value) {
-						return 'Please enter a project name';
-					}
-					project_name = value.replace('/', '');
-				}
-			}),
-		web: async () =>
-			await text({
-				message: 'Please enter the path to your web folder (web)',
-				initialValue: `${cwd}`,
-				validate: (value: string) => {
-					if (isCancel(value)) exit(0);
-					web_route = value;
-				}
-			}),
-		lib: async () =>
-			await text({
-				message: `Please enter the path to your lib folder`,
-				initialValue: `${web_route}/src/lib/${project_name}/${package_name}`,
-				validate: (value: string) => {
-					if (isCancel(value)) exit(0);
-				}
-			}),
-		public: async () =>
-			await text({
-				message: `Please enter the path to your public routes.`,
-				initialValue: `${web_route}/src/routes/(public)/${project_name}/${package_name}`,
-				validate: (value: string) => {
-					if (isCancel(value)) exit(0);
-				}
-			}),
-		private: async () =>
-			await text({
-				message: `Please enter the path to your private routes.`,
-				initialValue: `${web_route}/src/routes/(private)/${project_name}/${package_name}`,
-			}),
-		package: async () =>
-			await text({
-				message: "Please enter the path to your web's package.json",
-				initialValue: `${web_route}/package.json`
-			})
-	});
+          package_name = value.replace("/", "");
+        },
+      }),
+    project: async () =>
+      await text({
+        message: `Please enter the project name`,
+        validate: (value: string) => {
+          if (isCancel(value)) exit(0);
+          if (!value) {
+            return "Please enter a project name";
+          }
+          project_name = value.replace("/", "");
+        },
+      }),
+    web: async () =>
+      await text({
+        message: "Please enter the path to your web folder (web)",
+        initialValue: `${cwd}`,
 
-	return project;
+        validate: (value: string) => {
+          web_route = value;
+        },
+      }),
+    package: async () =>
+      await text({
+        message: "Please enter the path to your web's package.json",
+        initialValue: `${web_route}/package.json`,
+      }),
+    lib: async () =>
+      await text({
+        message: `Please enter the path to your lib folder`,
+        initialValue: `${web_route}/src/lib/${project_name}/${package_name}`,
+      }),
+  });
+
+  const havePublicRoute: boolean | symbol = await confirm({message: `Do you have public routes?`, inactive: 'No'});
+  let publicRoute: string | symbol | undefined;
+  if (havePublicRoute) {
+    publicRoute = await text({
+      message: `Please enter the path to your public routes.`,
+      initialValue: `${web_route}/src/routes/(public)/${project_name}/${package_name}`,
+    });
+
+    if (isCancel(publicRoute)) exit(0);
+  }
+
+  const havePrivateRoute: boolean | symbol = await confirm({message: `Do you have private routes?`, inactive: 'No'});
+  let privateRoute: string | symbol | undefined;
+  if (havePrivateRoute) {
+    privateRoute = await text({
+      message: `Please enter the path to your private routes.`,
+      initialValue: `${web_route}/src/routes/(private)/${project_name}/${package_name}`,
+    });
+
+    if (isCancel(privateRoute)) exit(0);
+  }
+
+  return {
+    ...project,
+    public: publicRoute as string,
+    private: privateRoute as string,
+  };
 }
